3e48e9a7914d1f419828de572c261cbcf6caa0bc
NgcLiem <ngocliemvo2025@gmail.com>
2025-12-29 03:26:07 +0700

    deploy

--- src/app/reset-password/page.jsx
+++ src/app/reset-password/page.jsx
@@ -1,5 +1,5 @@
 import { Suspense } from 'react';
-import ResetPasswordForm from './ResetPasswordForm'; 
+import ResetPasswordForm from './resetPasswordForm';
 
 export default function ResetPasswordPage() {
   return (
